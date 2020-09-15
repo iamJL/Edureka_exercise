8139febbe67e306579d72c8cd9e809efe1e0b814
iamJL <joylakra26@gmail.com>
2020-09-15 13:23:10 +0530

    Sample file for new branch

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class Sample{
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is the new branch created!");
+		System.out.println("This is another sample branch");
+	}
+}
